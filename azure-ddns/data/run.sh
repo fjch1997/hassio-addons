7252f05bb7a878d0dbff858605d53d438fd79c69
Jingchao Feng <fjch1997@outlook.com>
2024-07-13 23:06:24 -0700

    No single file

--- azure-ddns/data/run.sh
+++ azure-ddns/data/run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bashio
 set -e
 
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 while :
 do
-    ./Azure-DynDns --interface-name eth0 --dry-run
+    /root/.dotnet/dotnet /Azure-DynDns.dll --interface-name eth0 --dry-run
     sleep 300
 done
