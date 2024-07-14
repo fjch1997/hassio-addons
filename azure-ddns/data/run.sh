be3b85f38db4a08cd70b0cab3f9b23deca6e98cd
Jingchao Feng <fjch1997@outlook.com>
2024-07-14 00:51:21 -0700

    azure-ddns now working

--- azure-ddns/data/run.sh
+++ azure-ddns/data/run.sh
@@ -1,9 +1,28 @@
-#!/usr/bin/env bashio
+#!/usr/bin/with-contenv bashio
 set -e
 
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+CONFIG_PATH=/data/options.json
+
 while :
 do
-    /root/.dotnet/dotnet /Azure-DynDns.dll --interface-name eth0 --dry-run
+    if [ "$(bashio::config 'dryRun')" = true ] ; then
+        DryRun="--dyr-run"
+    else
+        DryRun=""
+    fi
+
+    /root/.dotnet/dotnet /Azure-DynDns.dll \
+    --tenant-id "$(bashio::config 'tenantId')" \
+    --subscription-id "$(bashio::config 'subscriptionId')" \
+    --resource-group "$(bashio::config 'resourceGroup')" \
+    --zone "$(bashio::config 'zone')" \
+    --record "$(bashio::config 'record')" \
+    --client-id "$(bashio::config 'clientId')" \
+    --client-secret "$(bashio::config 'clientSecret')" \
+    --interface-name "$(bashio::config 'interfaceName')" \
+    --interface-address-family $(bashio::config 'interfaceAddressFamily') \
+    --ttl $(bashio::config 'ttl') \
+    $DryRun
     sleep 300
 done
