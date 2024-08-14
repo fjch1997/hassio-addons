45531a1a9a72365e2406299ed079b6c945c6d1ee
Jingchao Feng <fjch1997@outlook.com>
2024-08-14 12:38:40 -0700

    This doesn't work

--- azure-ddns/data/run.sh
+++ azure-ddns/data/run.sh
@@ -4,35 +4,53 @@ set -e
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 CONFIG_PATH=/data/options.json
 
+if [ "$(bashio::config 'dryRun')" = true ] ; then
+    DryRun="--dry-run"
+else
+    DryRun=""
+fi
+
+InterfaceName=""
+for i in $(bashio::config "interfaceName"); do
+    if [[ -z "$InterfaceName" ]]; then
+        InterfaceName="--interface-name"
+    fi
+    InterfaceName="$InterfaceName \""$i"\""
+done
+
+InterfaceAddressFamily=""
+for i in $(bashio::config "interfaceAddressFamily"); do
+    if [[ -z "$InterfaceAddressFamily" ]]; then
+        InterfaceAddressFamily="--interface-address-family"
+    fi
+    InterfaceAddressFamily="$InterfaceAddressFamily $(echo $i | xargs)"
+done
+
 while :
 do
-    if [ "$(bashio::config 'dryRun')" = true ] ; then
-        DryRun="--dyr-run"
-    else
-        DryRun=""
-    fi
-
-    InterfaceName=""
-    for i in $(bashio::config "interfaceName"); do
-        InterfaceName+="--interface-name \"$i\" "
-    done
-
-    InterfaceAddressFamily=""
-    for i in $(bashio::config "interfaceAddressFamily"); do
-        InterfaceAddressFamily+="--interface-address-family \"$i\" "
-    done
-
-    /root/.dotnet/dotnet /Azure-DynDns.dll \
-    --tenant-id "$(bashio::config 'tenantId')" \
-    --subscription-id "$(bashio::config 'subscriptionId')" \
-    --resource-group "$(bashio::config 'resourceGroup')" \
-    --zone "$(bashio::config 'zone')" \
-    --record "$(bashio::config 'record')" \
-    --client-id "$(bashio::config 'clientId')" \
-    --client-secret "$(bashio::config 'clientSecret')" \
+    echo /root/.dotnet/dotnet /Azure-DynDns.dll \
+    --tenant-id \""$(bashio::config 'tenantId')"\" \
+    --subscription-id \""$(bashio::config 'subscriptionId')"\" \
+    --resource-group \""$(bashio::config 'resourceGroup')"\" \
+    --zone \""$(bashio::config 'zone')"\" \
+    --record \""$(bashio::config 'record')"\" \
+    --client-id \""$(bashio::config 'clientId')"\" \
+    --client-secret \""$(bashio::config 'clientSecret')"\" \
     $InterfaceName \
     $InterfaceAddressFamily \
-    --ttl $(bashio::config 'ttl') \
+    --ttl "$(bashio::config 'ttl')" \
+    $DryRun || true
+    /root/.dotnet/dotnet /Azure-DynDns.dll \
+    --tenant-id \""$(bashio::config 'tenantId')"\" \
+    --subscription-id \""$(bashio::config 'subscriptionId')"\" \
+    --resource-group \""$(bashio::config 'resourceGroup')"\" \
+    --zone \""$(bashio::config 'zone')"\" \
+    --record \""$(bashio::config 'record')"\" \
+    --client-id \""$(bashio::config 'clientId')"\" \
+    --client-secret \""$(bashio::config 'clientSecret')"\" \
+    $InterfaceName \
+    $InterfaceAddressFamily \
+    --ttl "$(bashio::config 'ttl')" \
     $DryRun || true
     sleep 300
 done
